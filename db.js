f55962fe6190eb6f4c3b9fb6e22aa65465b5766e
TanvirFahimBD <tanvirfahim6939@gmail.com>
2023-01-25 21:19:27 +0600

    pcProducts json updated �

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const todos = require("./data/db.json");
 const todosCopy = require("./data/copy.json");
-const products = require("./data/products.json");
+const pcProducts = require("./data/pcProducts.json");
 
 module.exports = {
   todos,
   todosCopy,
-  products,
+  pcProducts,
 };
